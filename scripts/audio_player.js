7f24be1ed70bcd36ffd816fe5bccc367110a1229
cmcfadzean <court@kajabi.com>
2019-03-10 13:10:21 -0700

    post snippits

--- scripts/audio_player.js
+++ scripts/audio_player.js
@@ -1,27 +1,3 @@
-$(function() {
-  var a = audiojs.createAll({
-    trackEnded: function() {
-      var next = $('.audio ol li.playing').next();
-      if (!next.length) next = $('.audio-player ol li').first();
-      next.addClass('playing').siblings().removeClass('playing');
-      audio.load($('a', next).attr('data-src'));
-      audio.play();
-    }
-  });
-  var audio = a[0];
-  if (audio) {
-    first = $('.audio ol a').attr('data-src');
-    $('.audio ol li').first().addClass('playing');
-    audio.load(first);
-    $('.audio ol li').click(function(e) {
-      e.preventDefault();
-      $(this).addClass('playing').siblings().removeClass('playing');
-      audio.load($('a', this).attr('data-src'));
-      audio.play();
-    });
-  }
-});
-
 // A cross-browser javascript shim for html5 audio
 (function(audiojs, audiojsInstance, container) {
   // Use the path to the audio.js file to create relative paths to the swf and player graphics
